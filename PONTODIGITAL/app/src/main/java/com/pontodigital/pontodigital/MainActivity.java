3df26b2a73acd4d94d064c8aec23f7d1d59242a9
Rafael Carvalho <rapha.pse@outlook.com>
2018-01-21 21:05:21 -0200

    Aplicativo ponto digital

--- PONTODIGITAL/app/src/main/java/com/pontodigital/pontodigital/MainActivity.java
+++ PONTODIGITAL/app/src/main/java/com/pontodigital/pontodigital/MainActivity.java
@@ -1,13 +1,8 @@
 package com.pontodigital.pontodigital;
 
-import android.content.Intent;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.transition.Slide;
-import android.transition.TransitionInflater;
-import android.transition.TransitionManager;
-import android.view.ViewGroup;
-import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,16 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        final ViewGroup root = (ViewGroup) findViewById(R.id.pd);
-        animation(root);
-
-
     }
 
-    public void animation(ViewGroup v){
-        TransitionManager.beginDelayedTransition(v, new Slide());
-        startActivity(new Intent(MainActivity.this, Acesso.class));
-        finish();
-    }
+
 }
