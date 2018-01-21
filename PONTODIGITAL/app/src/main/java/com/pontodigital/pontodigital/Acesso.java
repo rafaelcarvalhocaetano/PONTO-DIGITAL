291afcfacc3bd2e9996a078189ab5b1ab7e27f4f
Rafael Carvalho <rapha.pse@outlook.com>
2018-01-21 20:12:35 -0200

    Aplicativo ponto digital

---
+++ PONTODIGITAL/app/src/main/java/com/pontodigital/pontodigital/Acesso.java
@@ -0,0 +1,20 @@
+package com.pontodigital.pontodigital;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+
+/**
+ * Created by rafael on 21/01/18.
+ */
+
+public class Acesso extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acesso);
+    }
+}
--- PONTODIGITAL/app/src/main/java/com/pontodigital/pontodigital/MainActivity.java
+++ PONTODIGITAL/app/src/main/java/com/pontodigital/pontodigital/MainActivity.java
@@ -1,13 +1,34 @@
 package com.pontodigital.pontodigital;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.transition.Slide;
+import android.transition.TransitionInflater;
+import android.transition.TransitionManager;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        final ViewGroup root = (ViewGroup) findViewById(R.id.pd);
+        animation(root);
+
+
+
+
+    }
+
+    public void animation(ViewGroup v){
+        TransitionManager.beginDelayedTransition(v, Slide());
+        startActivity(new Intent(MainActivity.this, Acesso.class));
+        finish();
     }
 }
