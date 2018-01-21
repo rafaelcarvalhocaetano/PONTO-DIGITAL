bbe14b9223805ef7e07e62e30e3fbd0d84a785f8
Rafael Carvalho <rapha.pse@outlook.com>
2018-01-21 21:16:59 -0200

    Aplicativo ponto digital

--- PONTODIGITAL/app/src/main/java/com/pontodigital/pontodigital/MainActivity.java
+++ PONTODIGITAL/app/src/main/java/com/pontodigital/pontodigital/MainActivity.java
@@ -1,16 +1,34 @@
 package com.pontodigital.pontodigital;
 
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = (Button) findViewById(R.id.acesso);
+
+        final Intent i = new Intent(MainActivity.this, Acesso.class);
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(i);
+                finish();
+            }
+        });
 
     }
 
