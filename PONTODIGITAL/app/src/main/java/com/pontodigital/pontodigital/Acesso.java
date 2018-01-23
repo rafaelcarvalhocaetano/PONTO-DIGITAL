b50f685d0cc960239312393d46c77caa0bb391c1
Rafael Carvalho <rapha.pse@outlook.com>
2018-01-22 22:27:08 -0200

    Add Ponto digital

--- PONTODIGITAL/app/src/main/java/com/pontodigital/pontodigital/Acesso.java
+++ PONTODIGITAL/app/src/main/java/com/pontodigital/pontodigital/Acesso.java
@@ -1,8 +1,11 @@
 package com.pontodigital.pontodigital;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.view.View;
 
 
 /**
@@ -12,9 +15,23 @@ import android.support.v7.app.AppCompatActivity;
 public class Acesso extends AppCompatActivity {
 
 
+    private CardView cv;
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.acesso);
+
+        cv = (CardView) findViewById(R.id.card);
+
+        cv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Acesso.this, AcessoPosto.class);
+                startActivity(i);
+                finish();
+            }
+        });
     }
 }
