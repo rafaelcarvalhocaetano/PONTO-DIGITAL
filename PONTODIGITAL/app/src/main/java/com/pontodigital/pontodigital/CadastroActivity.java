b49ff0c1f0e22534707728846454a6fb3b422605
Rafael Carvalho <rapha.pse@outlook.com>
2018-03-13 22:14:47 -0300

    Add App PontoDigital

--- PONTODIGITAL/app/src/main/java/com/pontodigital/pontodigital/CadastroActivity.java
+++ PONTODIGITAL/app/src/main/java/com/pontodigital/pontodigital/CadastroActivity.java
@@ -2,7 +2,12 @@ package com.pontodigital.pontodigital;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
+
+import com.github.rtoshiro.util.format.MaskFormatter;
+import com.github.rtoshiro.util.format.SimpleMaskFormatter;
+import com.github.rtoshiro.util.format.text.MaskTextWatcher;
 
 /**
  * Created by rafael on 24/01/18.
@@ -10,11 +15,15 @@ import android.support.v7.app.AppCompatActivity;
 
 public class CadastroActivity extends AppCompatActivity {
 
+    private TextInputEditText cnpj;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cadastrar_empresa);
+
+        cnpj = (TextInputEditText) findViewById(R.id.cnpj);
 
         //XX.XXX.XXX/0001-XX  cnpj
         /*
@@ -22,5 +31,10 @@ public class CadastroActivity extends AppCompatActivity {
             MaskTextWatcher mtw = new MaskTextWatcher(myEditText, msf)
             myEditText.addTextChangedListener(mtw);
          */
+
+        SimpleMaskFormatter SimpleCNPJ = new SimpleMaskFormatter("NN.NNN.NNN/NNNN-NN");
+        MaskTextWatcher mCNPJ = new MaskTextWatcher(cnpj, SimpleCNPJ);
+
+        cnpj.addTextChangedListener( mCNPJ );
     }
 }
--- PONTODIGITAL/app/src/main/java/com/pontodigital/pontodigital/LoginActivity.java
+++ PONTODIGITAL/app/src/main/java/com/pontodigital/pontodigital/LoginActivity.java
@@ -6,7 +6,11 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import com.github.rtoshiro.util.format.SimpleMaskFormatter;
+import com.github.rtoshiro.util.format.text.MaskTextWatcher;
 
 
 /**
@@ -16,6 +20,7 @@ import android.widget.TextView;
 public class LoginActivity extends AppCompatActivity {
 
     private TextView cadastrar;
+    private EditText cnpj;
 
 
     @Override
@@ -24,6 +29,12 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         cadastrar = (TextView) findViewById(R.id.cadastrar_empresa);
+        cnpj = (EditText) findViewById(R.id.edit_cnpj);
+
+        SimpleMaskFormatter SimpleCNPJ = new SimpleMaskFormatter("NN.NNN.NNN/NNNN-NN");
+        MaskTextWatcher mCNPJ = new MaskTextWatcher(cnpj, SimpleCNPJ);
+
+        cnpj.addTextChangedListener( mCNPJ );
 
 
         cadastrar.setOnClickListener(new View.OnClickListener() {
