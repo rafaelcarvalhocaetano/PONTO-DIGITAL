29875722d4dbffbcf9fad4fe4d8d28f2a46c0b3d
Rafael Carvalho <rapha.pse@outlook.com>
2018-03-13 21:21:10 -0300

    Add App PontoDigital

--- PONTODIGITAL/app/src/main/java/com/pontodigital/pontodigital/LoginActivity.java
+++ PONTODIGITAL/app/src/main/java/com/pontodigital/pontodigital/LoginActivity.java
@@ -1,19 +1,28 @@
 package com.pontodigital.pontodigital;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.view.View;
+
 
 /**
- * Created by rafael on 22/01/18.
+ * Created by rafael on 21/01/18.
  */
 
-public class AcessoPosto extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.acesso_posto);
+        setContentView(R.layout.activity_login);
+
+
+
+
+
     }
 }
--- PONTODIGITAL/app/src/main/java/com/pontodigital/pontodigital/MainActivity.java
+++ PONTODIGITAL/app/src/main/java/com/pontodigital/pontodigital/MainActivity.java
@@ -5,16 +5,11 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.TextView;
 
 //config. parse
 
 import com.parse.Parse;
-import com.parse.ParseException;
-import com.parse.ParseObject;
-import com.parse.ParseUser;
-import com.parse.ParseInstallation;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,8 +21,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn = (TextView) findViewById(R.id.acesso);
-
 
         Parse.initialize(this);
 
@@ -36,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         temp.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent i = new Intent(MainActivity.this, Acesso.class);
+                Intent i = new Intent(MainActivity.this, LoginActivity.class);
                 startActivity( i );
                 finish();
             }
