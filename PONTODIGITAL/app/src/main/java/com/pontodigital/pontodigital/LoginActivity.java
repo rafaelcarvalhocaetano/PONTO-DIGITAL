477d5d8138662822f4b276a91cd72ff061b63147
Rafael Carvalho <rapha.pse@outlook.com>
2018-03-13 21:54:09 -0300

    Add App PontoDigital

--- PONTODIGITAL/app/src/main/java/com/pontodigital/pontodigital/LoginActivity.java
+++ PONTODIGITAL/app/src/main/java/com/pontodigital/pontodigital/LoginActivity.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.view.View;
+import android.widget.TextView;
 
 
 /**
@@ -14,11 +15,26 @@ import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private TextView cadastrar;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        cadastrar = (TextView) findViewById(R.id.cadastrar_empresa);
+
+
+        cadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, CadastroActivity.class));
+                finish();
+            }
+        });
+
+
 
 
 
